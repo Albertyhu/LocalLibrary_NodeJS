7577b3342530e7732ea12104d32d4f446ee8b471
unknown <hualbert@rocketmail.com>
2022-12-21 17:01:00 -0800

    first commit

--- controllers/bookInstanceController.js
+++ controllers/bookInstanceController.js
@@ -1,8 +1,26 @@
 const BookInstance = require("../models/bookinstance");
 
 // Display list of all BookInstances.
+//book is one of the fields in the schema BookInstanceSchema 
+/* The method "populate" helps retrieve the entire document that contains 
+ * the data we want for the field "book".
+ * The method 'exec' is used to execute a query and has two parameters. 
+ * The first handles errors and the second handles returning the result if the 
+ * operation is successful. 
+ * */
 exports.bookinstance_list = (req, res) => {
-  res.send("NOT IMPLEMENTED: BookInstance list");
+    BookInstance.find()
+        .populate("book")
+        .exec(function (err, list_bookinstances) {
+            if (err) {
+                return next(err);
+            }
+            // Successful, so render
+            res.render("bookinstance_list", {
+                title: "Book Instance List",
+                bookinstance_list: list_bookinstances,
+            });
+        });
 };
 
 // Display detail page for a specific BookInstance.
